f419bb8c3f5145bf1ec0b7924424bbca668f0aaf
james ngacha <jazooh@gmail.com>
2018-07-02 16:08:53 +0300

    7

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb://jazooh:TREster50&@ds125211.mlab.com:25211/dbngacha'
+      'mongodb://mimi:mimi1234@ds125211.mlab.com:25211/dbngacha'
   }
 
 } else {
